a77dcd2555cdf0b33b3a8f7bbbb0a38c2829e7b6
HenryPahn <thoang0303@gmail.com>
2025-01-21 21:33:51 -0500

    Rewrite HTTP response - using createSuccessResponse() and createErrorResponse()

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,13 +1,18 @@
 // src/routes/api/get.js
 
+// Functions return successful responses 
+const { createSuccessResponse } = require('../../response')
+
 /**
  * Get a list of fragments for the current user
  */
 module.exports = (req, res) => {
+  const fragments = []
+
+  const successResponse = createSuccessResponse({
+    fragments: fragments,
+  })
+
   // TODO: this is just a placeholder. To get something working, return an empty array...
-  res.status(200).json({
-    status: 'ok',
-    // TODO: change me
-    fragments: [],
-  });
+  res.status(200).json(successResponse);
 };
