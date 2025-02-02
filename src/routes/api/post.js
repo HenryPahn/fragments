45fcdea3b303748fe868d83582fcfff0cc1093b1
HenryPahn <thoang0303@gmail.com>
2025-02-02 15:12:43 -0500

    Add POST /fragments route, and unit tests for it

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -2,33 +2,79 @@
 
 // Functions return successful responses 
 const { createSuccessResponse, createErrorResponse } = require('../../response')
-
 const { Fragment } = require('../../model/fragment')
-
 const logger = require('../../logger')
+
+const contentType = require('content-type');
 
 /**
  * Creates a new fragment for the current user.
  */
-module.exports = (req, res) => {
-  // Check if the current npm script is 'dev' or 'debug'
-  const isDebugging = process.env.npm_lifecycle_event === 'dev' || process.env.npm_lifecycle_event === 'debug';
+module.exports = async (req, res) => {
+  try {
+    // Parse content-type header 
+    const fragmentType = contentType.parse(req).type;
 
-  // if user is debugging, print all environment variables to check if any is missing	  
-  if (isDebugging) {
-    logger.debug(`User token: ${req.user}`);
-    logger.debug(`Description: ${req.body}`);
+    if (!fragmentType || !Fragment.isSupportedType(fragmentType)) {
+      throw { message: `type are mandatory and must be supported, got type=${fragmentType}`, status: 415 };
+    }
+
+    const content = req.body; 
+
+    if(!content || content.length === 0) {
+      throw { message: `data are mandatory and not empty, got data=${content}`, status: 400 };
+    }
+
+    // Check if the current npm script is 'dev' or 'debug'
+    const isDebugging = process.env.npm_lifecycle_event === 'dev' || process.env.npm_lifecycle_event === 'debug';
+
+    // if user is debugging, print all environment variables to check if any is missing	  
+    if (isDebugging) {
+      logger.debug(`Request to POST /fragments:
+- User token: ${req.user}
+- Description: ${req.body}
+- Type: ${fragmentType}
+- URL: ${req.headers.host}`);
+    }
+
+    // Get data size
+    const dataSize = Buffer.byteLength(req.body, "utf-8");
+
+    // create a new fragment object
+    const fragment = new Fragment({ ownerId: req.user, type: fragmentType, size: dataSize });
+
+    // store fragment object into DB
+    await fragment.save();
+
+    // Get the API base URL from environment or fallback to req.headers.host
+    const apiBaseUrl = process.env.API_URL || `http://${req.headers.host}`;
+
+    // Construct the Location URL
+    const locationUrl = new URL(`/v1/fragments/${fragment.id}`, apiBaseUrl).href;
+
+    // Set the Location header
+    res.setHeader("Location", locationUrl);
+
+    const successResponse = createSuccessResponse({
+      fragments: fragment,
+    })
+
+    res.status(201).json(successResponse);
+  } catch (err) {
+    const status = err.status || 500;
+    const message = err.message || 'unable to process request';
+
+    const errorMessage = createErrorResponse(status, message)
+
+    if (status === 415) {
+      logger.error({ err }, `Unsupported type`);
+    }
+
+    if (status === 400) {
+      logger.error({ err }, `Missing data`);
+    }
+
+    res.status(status).json(errorMessage);
   }
-  
-  // logger.debug(req.user);
-  // const fragment = new Fragment();
-
-  const mess = "Creating";
-
-  const successResponse = createSuccessResponse({
-    status: mess,
-  })
-
-  res.status(200).json(successResponse);
 };
 
---
+++ tests/unit/post.test.js
@@ -0,0 +1,53 @@
+// tests/unit/post.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('POST /v1/fragments', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () => request(app).post('/v1/fragments').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () => request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
+
+  // If the authenticated user forgot to pass content-type 
+  test('missing content-type', async () =>  {
+    await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .expect(500)
+  });
+
+  // If the authenticated user passed unsupported content-type 
+  test('unsupported content-type', async () =>  {
+    await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'application/unsupported-type')
+      .expect(415)
+  });
+
+  // If the authenticated user forgot to pass data 
+  test('missing data', async () =>  {
+    await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send()
+      .expect(400)
+  });
+
+  // If the authenticated user passed all correct data 
+  test('authenticated user sends request to POST /fragments', async () =>  {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is a fragment')
+
+    expect(res.body.status).toEqual('ok');
+    expect(res.body.fragments.type).toEqual('text/plain');
+    expect(res.body.fragments.size).toBe(18);
+  });
+});
